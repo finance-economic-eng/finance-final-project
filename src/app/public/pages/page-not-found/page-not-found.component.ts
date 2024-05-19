c08b982dfe93b4dc5613ba2a243b3ab1ef2ddb6d
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-05-19 10:45:04 -0500

    feat(static-content-pt1): added  home and page-not-found components

--- src/app/public/pages/page-not-found/page-not-found.component.ts
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit{
+  invalidUrl: string;
+  constructor(private route: ActivatedRoute, private router: Router){
+    this.invalidUrl = '';
+  }
+
+  onNavigateHome(){
+    this.router.navigate(['home']).then();
+  }
+  ngOnInit(): void {
+    this.invalidUrl = this.route.snapshot.url[0].path;
+  }
 
 }
