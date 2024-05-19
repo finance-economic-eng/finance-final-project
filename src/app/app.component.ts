6782074cb2ff39eb5dddda5ad2cfb0b206e9cb2b
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-05-19 11:46:54 -0500

    feat(static-content-pt2): router finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'final-project';
+  title = 'MtPlatform';
+  options = [
+    {path: '/home', title: 'Home'},
+  ]
 }
